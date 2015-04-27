87ea59027144c466480fedad1e02dc15f96565f0
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2015-04-27 09:06:48 +0800

    Filter data before ploting

--- lib/voiceprint/audio.rb
+++ lib/voiceprint/audio.rb
@@ -1,39 +1,29 @@
 module Voiceprint
-
   module Audio
-    include RubyAudio
+    class << self
+      include RubyAudio
 
-    def self.open(file)
-      Sound.open(file)
-    end
-
-    def self.play(file)
-      # Play sound (.wav file supported)
-      system("play #{file}");
-    end
-
-    def self.disp_file_info(file)
-      snd = Sound.open(file)
-
-      sample_rate = snd.info.samplerate
-      channels = snd.info.channels
-      len_secs = snd.info.length.round(2)
-      puts "Sample rate: #{sample_rate}"
-      puts "Channels: #{channels}"
-      puts "Length: #{len_secs} secs"
-
-      snd.close
-    end
-
-    def self.buf2narray(a)
-      n = a.size
-      na = NArray.float(n)
-      (0..n-1).each do |index|
-        na[index] = a[index]
+      def self.open(file)
+        Sound.open(file)
       end
-      na
+
+      def self.play(file) # Play sound (.wav file supported)
+        system("play #{file}");
+      end
+
+      def self.disp_file_info(file)
+        snd = Sound.open(file)
+
+        sample_rate = snd.info.samplerate
+        channels = snd.info.channels
+        len_secs = snd.info.length.round(2)
+        puts "Sample rate: #{sample_rate}"
+        puts "Channels: #{channels}"
+        puts "Length: #{len_secs} secs"
+
+        snd.close
+      end
+
     end
-
   end
-
 end
--- lib/voiceprint/plot.rb
+++ lib/voiceprint/plot.rb
@@ -4,6 +4,7 @@ module Voiceprint
     class << self
 
       def plot(*args, **opts)
+
         case args.size
         when 1
           y = args.first
@@ -20,6 +21,11 @@ module Voiceprint
           raise "Invalid arguments!"
         end
 
+        # Reduce data
+        x = select_sample(x)
+        y = select_sample(y)
+
+        # Make plot
         plot = Nyaplot::Plot.new
         df = Nyaplot::DataFrame.new({x:x, y:y})
         plot.add_with_df(df, :line, :x, :y)
@@ -29,6 +35,20 @@ module Voiceprint
         plot.x_label(xlabel)
         plot.y_label(ylabel)
         plot.show
+      end
+
+      private
+
+      def select_sample(array)
+        num_total = array.length
+        num_threshold = 8192
+        num_plot = 1024
+        num_sample = num_total / num_plot
+
+        if num_total >= num_threshold
+          array = array.select.with_index {|e, i| i % num_sample == 0}
+        end
+        array
       end
 
     end
